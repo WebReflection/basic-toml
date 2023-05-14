08fb4ac3b08c90a3d6d812e075685ffac5fee54b
webreflection <andrea.giammarchi@gmail.com>
2023-05-14 17:49:01 +0200

    simplified cleanup and revival of entries

--- esm/index.js
+++ esm/index.js
@@ -1,41 +1,19 @@
 /*! (c) Andrea Giammarchi - ISC */
 
 const {isArray} = Array;
-
-/**
- * Remove multi-line arrays' entries.
- * @param {string} str the TOML to sanitize
- * @returns {string} the sanitized TOML
- */
-const noMultiLineArray = str => str
-  .replace(/,\s*[\r\n]+/g, ', ')
-  .replace(/\[\s*[\r\n]+/g, '[')
-  .replace(/[\r\n]+\s*]/g, ']')
-;
+const {parse: jsonParse} = JSON;
 
 /**
  * Given a `'string'` or a `"string"` returns a JSON compatible string.
  * @param {string} str a TOML entry to parse
  * @returns {string}
  */
-const getValue = str => {
-  const strings = [];
-  const dates = [];
-  return JSON.parse(
-    str
-      .replace(
-        /(["'])(?:(?=(\\?))\2.)*?\1/g,
-        value => `"s${strings.push(value.slice(1, -1)) - 1}"`
-      )
-      .replace(
-        /\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:-\d{2}:\d{2})?)?/g,
-        value => `"d${dates.push(new Date(value)) - 1}"`
-      ),
-    (_, value) => typeof value === 'string' ?
-      (value[0] === 's' ? strings : dates)[value.slice(1)] :
-      value
-  );
-};
+const getValue = (str, foreign) => jsonParse(
+  str,
+  (_, value) => typeof value === 'string' ?
+    foreign[value[0]][value.slice(1)] :
+    value
+);
 
 /**
  * Crawl the `json` object via the given array of keys and handle array entries.
@@ -57,19 +35,47 @@ const getPath = (keys, entry, asArray) => {
 };
 
 /**
+ * Given a TOML text, removes stirngs and dates for easier parsing +
+ * remove multi-line arrays to not need evaluation.
+ * @param {string} toml the TOML text to map
+ * @param {string[]} strings mapped strings
+ * @param {Date[]} dates mapped Dates
+ * @returns {[string, {s: string[], d: Date[]}]}
+ */
+const mapForeign = (toml, strings, dates) => [
+  toml
+    // map strings in the TOML
+    .replace(
+      /(["'])(?:(?=(\\?))\2.)*?\1/g,
+      value => `"s${strings.push(value.slice(1, -1)) - 1}"`
+    )
+    // map dates in the TOML
+    .replace(
+      /\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}(?:-\d{2}:\d{2})?)?/g,
+      value => `"d${dates.push(new Date(value)) - 1}"`
+    )
+    // avoid multi-line array entries
+    .replace(/,\s*[\r\n]+/g, ', ')
+    .replace(/\[\s*[\r\n]+/g, '[')
+    .replace(/[\r\n]+\s*]/g, ']'),
+  {s: strings, d: dates}
+];
+
+/**
  * Given a simple subset of a TOML file, returns its JS equivalent.
- * @param {string} text the TOML text to parse
+ * @param {string} toml the TOML text to parse
  * @returns {object} the TOML equivalent as JSON serializable
  */
-export const parse = text => {
+export const parse = toml => {
+  const [text, foreign] = mapForeign(toml, [], []);
   const json = {};
   let entry = json;
-  for (let line of noMultiLineArray(text).split(/[\r\n]+/)) {
+  for (let line of text.split(/[\r\n]+/)) {
     if ((line = line.trim()) && !line.startsWith('#')) {
       if (/^(\[+)(.*?)\]+/.test(line))
         entry = getPath(RegExp.$2.trim().split('.'), json, RegExp.$1 !== '[');
       else if (/^(\S+?)\s*=([^#]+)/.test(line))
-        entry[RegExp.$1] = getValue(RegExp.$2.trim());
+        entry[RegExp.$1] = getValue(RegExp.$2.trim(), foreign);
     }
   }
   return json;
